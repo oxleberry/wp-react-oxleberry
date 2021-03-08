1fc2faee0c8d02904e8daed712055a7da96e1f7a
Sharon Paz <oxleberry@gmail.com>
2021-03-07 18:19:14 -0800

    Adding Sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/App.scss';
+// import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="header">
+        <h1>Hello Oxleberry</h1>
+      </header>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }

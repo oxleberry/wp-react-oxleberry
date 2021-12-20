1247ed4d0324e2d024db540e930331660c5a989c
Sharon Paz <oxleberry@gmail.com>
2021-12-20 15:28:51 -0800

    remove iMessage sticker page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './styles/App.scss';
 import Home from './pages/Home.js';
 import CreativeCoding from './pages/CreativeCoding.js';
-import IMessageStickers from './pages/IMessageStickers.js';
 import About from './pages/About.js';
 import Contact from './pages/Contact.js';
 import Nav from './components/Nav.js';
@@ -17,11 +16,9 @@ function App() {
           <div className="content">
             <Switch>
               <Route exact path="/" component={Home}></Route>
-              <Route path="/2021_website" component={Home}></Route>
-              <Route path="/home" component={Home}></Route>
+              <Route path="/react" component={Home}></Route>
               <Route path="/portfolio" component={Home}></Route>
               <Route path="/creative-coding" component={CreativeCoding}></Route>
-              <Route path="/imessage-stickers" component={IMessageStickers}></Route>
               <Route path="/about" component={About}></Route>
               <Route path="/contact" component={Contact}></Route>
             </Switch>
